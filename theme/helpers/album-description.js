8ebe54f11bfd4f72b7cdc41f8a2919d7464fa657
nukdokplex <nukdokplex@nukdokplex.ru>
2026-07-02 09:45:43 +0500

    format

--- theme/helpers/album-description.js
+++ theme/helpers/album-description.js
@@ -1,15 +1,20 @@
-const fs = require('node:fs')
-const path = require('node:path')
+const fs = require("node:fs");
+const path = require("node:path");
 
 module.exports = function (options) {
-  const album = options.data.root.album
-  if (album.home) return ''
+  const album = options.data.root.album;
+  if (album.home) return "";
 
-  const albumDir = path.dirname(album.path)
-  const descPath = path.join(process.cwd(), 'gallery', albumDir, 'description.md')
+  const albumDir = path.dirname(album.path);
+  const descPath = path.join(
+    process.cwd(),
+    "gallery",
+    albumDir,
+    "description.md",
+  );
   try {
-    return fs.readFileSync(descPath, 'utf-8').trim()
+    return fs.readFileSync(descPath, "utf-8").trim();
   } catch (e) {
-    return ''
+    return "";
   }
-}
+};
